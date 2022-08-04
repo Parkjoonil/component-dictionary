33a91923dad8504881a1b3fe057cd25420c06ee2
Parkjoonil <106866130+Parkjoonil@users.noreply.github.com>
2022-08-04 23:53:28 +0900

    route setting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GoogleMapComponent } from './google-map/google-map.component';
 import { UploadImageComponent } from './upload-image/upload-image.component';
 import { WeatherComponent } from './weather/weather.component';
+import { YoutubeComponent } from './youtube/youtube.component';
 
 const routes: Routes = [
   {
@@ -11,7 +13,15 @@ const routes: Routes = [
   {
     path: 'weather',
     component: WeatherComponent
-  }
+  },
+  {
+    path: 'youtube',
+    component: YoutubeComponent
+  },
+  {
+    path: 'google-map',
+    component: GoogleMapComponent
+  },
 ];
 
 @NgModule({
