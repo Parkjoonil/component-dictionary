857c7911a77ebb01c234f893e747f0e4fe2fc8cb
Parkjoonil <106866130+Parkjoonil@users.noreply.github.com>
2022-08-06 23:55:27 +0900

    youtube component 수정

--- src/app/youtube/youtube.component.ts
+++ src/app/youtube/youtube.component.ts
@@ -32,9 +32,30 @@ export class YoutubeComponent implements OnInit, AfterViewInit {
             distinctUntilChanged(),
         )
         .subscribe(
-        value => { this.search.emit(this.inputElement.nativeElement.value)}
+          this.handleSearch(this.inputElement.nativeElement.value);
       )
     }
-  } 
+  }
+
+  handleSearch(inputValue: string) {
+    this.loading = true;
+    this.youtubeService.getVideos(inputValue)
+      .subscribe((items: any) => {
+        console.log(items)
+        this.videos = items.map((item: any) => ({
+          title: item.snippet.title,
+          videoId: item.id.videoId,
+          videoUrl: `https://www.youtube.com/watch?v=${item.id.videoId}`,
+          channelId: item.snippet.channelId,
+          channelUrl: `https://www.youtube.com/channel/${item.snippet.channelId}`,
+          channelTitle: item.snippet.channelTitle,
+          description: item.snippet.description,
+          publishedAt: new Date(item.snippet.publishedAt),
+          thumbnail: item.snippet.thumbnails.high.url
+        }));
+        this.inputTouched = true;
+        this.loading = false;
+      });
+    }
 
 }
