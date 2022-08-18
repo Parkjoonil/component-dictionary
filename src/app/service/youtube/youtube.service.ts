677f2658b5947f151f8f6ce6500ba235a55771bc
Parkjoonil <106866130+Parkjoonil@users.noreply.github.com>
2022-08-19 00:38:57 +0900

    youtube component 수정

--- src/app/service/youtube/youtube.service.ts
+++ src/app/service/youtube/youtube.service.ts
@@ -6,14 +6,25 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class YoutubeService {
-  private API_URL = 'https://www.googleapis.com/youtube/v3/search';
   private API_KEY = 'AIzaSyBJkA_NBssyQV_Ps31PKm6tw5ZExE7cf34';
+  private API_URL = 'https://www.googleapis.com/youtube/v3';
+
   constructor(private http: HttpClient) {}
   getVideos(query: string): Observable <any> {
-    const url = `${this.API_URL}?q=${query}&key=${this.API_KEY}&part=snippet&type=video&maxResults=10`;
+    const url = `${this.API_URL}/search?q=${query}&key=${this.API_KEY}&part=snippet&type=video&maxResults=5`;
     return this.http.get(url)
       .pipe(
         map((response: any) => response.items)
       );
   }
+
+  getChannelVideos(channelId : string): Observable <any> {  
+    
+    const url = `${this.API_URL}/search?&key=${this.API_KEY}&part=snippet&type=video&order=date&channelId=${channelId}&maxResults=5`;
+    return this.http.get(url)
+      .pipe(
+        map((response: any) => response.items)
+      );
+  }
+
 }
