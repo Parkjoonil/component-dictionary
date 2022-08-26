32d3ea956d6c3a0c27df3ed10b75f85b42a53a2c
joonil <joonil@lepisode.team>
2022-08-26 23:56:52 +0900

    model service 작성

--- src/app/modal/modal.service.ts
+++ src/app/modal/modal.service.ts
@@ -1,4 +1,5 @@
 import { Overlay } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
 import { Injectable, Injector } from '@angular/core';
 
 @Injectable({
@@ -6,8 +7,25 @@ import { Injectable, Injector } from '@angular/core';
 })
 export class ModalService {
 
+  component: any;
+
   constructor(
     private injector: Injector,
     private overlay: Overlay
   ) { }
+
+  openOverlay(): void {
+    const overlayRef = this.overlay.create({
+      porisitonStrategy: this.overlay.position().global(),
+      hasBackdrop: true,
+      backdropClass: 'bg-black-50',
+      scrollStrategy: this.overlay.scrollStrategies.block(),
+    });
+
+    overlayRef.attach(this.component, null);
+
+    overlayRef.backdropClick().subscribe(_ => {
+      overlayRef.dispose();
+    })
+  }
 }
