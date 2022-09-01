ff31b30125b851867e2666ad851d08f620cd7a5c
joonil <joonil@lepisode.team>
2022-09-02 00:24:30 +0900

    youtube setting 수정

--- src/app/youtube/settings-youtube/settings-youtube.component.ts
+++ src/app/youtube/settings-youtube/settings-youtube.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSliderChange } from '@angular/material/slider';
 
 @Component({
   selector: 'app-settings-youtube',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsYoutubeComponent implements OnInit {
 
-  constructor() { }
+  videoNums : number;
+
+  constructor(
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onInputChange(event: MatSliderChange) {
+    this.videoNums = event.value;
   }
 
   closeModal() {
