a40f4410706be3f27ca2e071d046033572831e5d
joonil <joonil@lepisode.team>
2022-10-04 23:58:30 +0900

    youtube channel 수정

--- src/app/youtube/channels-youtube/channels-youtube.component.ts
+++ src/app/youtube/channels-youtube/channels-youtube.component.ts
@@ -59,7 +59,7 @@ export class ChannelsYoutubeComponent implements OnInit {
   }
 
   deleteChannel(youtubeChannelId: Dashboard) {
-    
+    this.youtubeChannelIds.filter((element) => element !== youtubeChannelId)
   }
 
   addChannel(searchResult: ChannelInfo) {
